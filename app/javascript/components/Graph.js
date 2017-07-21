c3c558df0e3172001bffb7b748c0e3aff26ce820
Nitin Reddy <nitin.reddy@weboniselab.com>
2017-07-21 15:36:51 +0530

    Alchemyjs options have been added

--- app/javascript/components/Graph.js
+++ app/javascript/components/Graph.js
@@ -12,7 +12,7 @@ var Graph = React.createClass({
 				"nodes": [
 					{
 						"id": "s1",
-						"caption": "192.168.0.20",
+						"caption": "Kyle",
 						"role": "http_server",
 						"memoryUsage": 50,
 						"root": true
@@ -22,100 +22,72 @@ var Graph = React.createClass({
 						"caption": "192.168.0.21",
 						"role": "http_server",
 						"memoryUsage": 22,
-						"root": true
-					},
-					{
-						"id": "s3",
-						"caption": "192.168.0.22",
-						"role": "http_server",
-						"memoryUsage": 95,
-						"root": true
-					},
-					{
-						"id": "s4",
-						"caption": "192.168.2.4",
-						"role": "neo4j",
-						"memoryUsage": 10,
 						"root": false
 					},
-					{
-						"id": "s5",
-						"caption": "192.168.2.6",
-						"role": "postgres",
-						"memoryUsage": 30,
-						"root": false
-					},
-					{
-						"id": "s6",
-						"caption": "192.168.2.7",
-						"role": "db_backups",
-						"memoryUsage": 10,
-						"root": false
-					},
-					{
-						"id": "s7",
-						"caption": "192.168.2.12",
-						"role": "load balancer",
-						"memoryUsage": 42,
-						"root": false
-					}
+					// {
+					// 	"id": "s3",
+					// 	"caption": "192.168.0.22",
+					// 	"role": "http_server",
+					// 	"memoryUsage": 95,
+					// 	"root": false
+					// },
+					// {
+					// 	"id": "s4",
+					// 	"caption": "192.168.2.4",
+					// 	"role": "neo4j",
+					// 	"memoryUsage": 10,
+					// 	"root": false
+					// },
+					// {
+					// 	"id": "s5",
+					// 	"caption": "192.168.2.6",
+					// 	"role": "postgres",
+					// 	"memoryUsage": 30,
+					// 	"root": false
+					// }
 				],
 				"edges": [
+
 					{
-						"source": "s7",
+						"source": "s2",
 						"target": "s1",
-						"load": 5
+						"load": 'REPORTS_TO'
 					},
-					{
-						"source": "s7",
-						"target": "s2",
-						"load": 2
-					},
-					{
-						"source": "s7",
-						"target": "s3",
-						"load": 2
-					},
-					{
-						"source": "s1",
-						"target": "s4",
-						"load": 4
-					},
-					{
-						"source": "s1",
-						"target": "s5",
-						"load": 1
-					},
-					{
-						"source": "s2",
-						"target": "s4",
-						"load": 1
-					},
-					{
-						"source": "s2",
-						"target": "s5",
-						"load": 1
-					},
-					{
-						"source": "s3",
-						"target": "s4",
-						"load": 0
-					},
-					{
-						"source": "s3",
-						"target": "s5",
-						"load": 1
-					},
-					{
-						"source": "s4",
-						"target": "s6",
-						"load": 0
-					},
-					{
-						"source": "s5",
-						"target": "s6",
-						"load": 0
-					}
+					// {
+					// 	"source": "s4",
+					// 	"target": "s1",
+					// 	"load": 'REPORTS_TO'
+					// },
+					// {
+					// 	"source": "s1",
+					// 	"target": "s5",
+					// 	"load": 1
+					// },
+					// {
+					// 	"source": "s2",
+					// 	"target": "s4",
+					// 	"load": 1
+					// },
+					// {
+					// 	"source": "s2",
+					// 	"target": "s5",
+					// 	"load": 1
+					// },
+					// {
+					// 	"source": "s3",
+					// 	"target": "s4",
+					// 	"load": 0
+					// },
+					// {
+					// 	"source": "s3",
+					// 	"target": "s5",
+					// 	"load": 1
+					// },
+					// {
+					// 	"source": "s4",
+					// 	"target": "s5",
+					// 	"load": 0
+					// }
 
 				]
 			},
@@ -130,28 +102,26 @@ var Graph = React.createClass({
 			nodeCaptionsOnByDefault: true,
 			nodeStyle: {
 				"all": {
-					"borderColor": "#127DC1",
-					"borderWidth": function (d, radius) {
-						return radius / 4
-					},
+					"borderWidth": 1,
 					"color": function (d) {
 						return "rgba(104, 185, 254, " +
 							(d.getProperties().memoryUsage / 100) + " )"
 					},
-					"radius": function (d) {
-						if (d.getProperties().root)
-							return 15; else return 10
+					"radius": 30,
+					"captionColor": "#000",
+					"highlighted": {
+						"color" : "#EEEEFF"
 					},
 				},
-				"http_server": {
-					"color": "#000067",
-					"selected": {
-						"color": "#ffffff",
-					},
-					"highlighted": {
-						"color": "#b4dcff"
-					}
-				}
+				// "http_server": {
+				// 	"color": "#000067",
+				// 	"selected": {
+				// 		"color": "#ffffff",
+				// 	},
+				// 	"highlighted": {
+				// 		"color": "#b4dcff"
+				// 	}
+				// }
 			},
 			edgeStyle: {
 				"all": {
@@ -159,7 +129,7 @@ var Graph = React.createClass({
 					"color": "#000",
 				}
 			},
-			edgeCaption: 'load'
+			edgeCaption: 'load',
 		};
 
 		alchemy = new Alchemy(config)
